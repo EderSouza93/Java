1263cc30315b7914eb99a99858dfa10ccb1d5ea7
EderSouza93 <ederaldo.souza93@gmail.com>
2022-09-09 22:28:05 -0300

    First steps with Java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+            String nome = "Eder";
+            System.out.println("Olá, " + nome);
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+
+    public static void main(String[] args){
+//definido a variável numérica onde int é a mais usual e utiliza números inteiros
+        //já float abranges os números reais
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+
+
+    }
+}
