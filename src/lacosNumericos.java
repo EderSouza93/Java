d2c0dd88218fc97b8812dca0e2ba733a780c5856
EderSouza93 <84224107+EderSouza93@users.noreply.github.com>
2022-09-16 22:12:50 -0300

    Laços Numerics

---
+++ src/lacosNumericos.java
@@ -0,0 +1,13 @@
+public class lacosNumericos {
+
+    public static void main(String[] args) {
+        // 1 2 3 4 5 6 7 8 9 10
+        // para uma variável que inicia em 1, e vai até 10, mudando 1-por-1, faça:
+        for (int i =1; i <=10; i++) {
+            //podemos alterar a quantidade de lacos no valor após o <=
+            //podemos alterar a quantidade de espaço na contagem alterando i++ por i+={numero}
+            System.out.println(i);
+        }
+    }
+
+}
